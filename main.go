7141d341cd139a3870f2d8e1b1bf8b2268b4037f
Mike Hoskins <mrh@devopsdreams.io>
2020-05-25 16:53:47 -0400

    implement GET and POST events endpoints

--- main.go
+++ main.go
@@ -2,30 +2,56 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-var events = make(map[string]string)
+type event struct {
+	Timestamp string `json:"last_update,omitempty"`
+	// Service   string `json:"service,omitempty"`
+	Status string `json:"status,omitempty"`
+}
 
+// Events serves as our "status database" and
+// maps service names to events
+var Events = make(map[string]event)
+
+// GET /events
 func getEvents(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"message": "hello world",
+		"events": Events,
 	})
 }
 
+// GET /events/:id
 func getEvent(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "not implemented",
 	})
 }
 
+// POST /events
 func createEvent(c *gin.Context) {
+	service := c.PostForm("service")
+	status := c.PostForm("status")
+
+	if service != "" {
+		e := new(event)
+		e.Status = string(status)
+		t := time.Now().UTC()
+		e.Timestamp = t.String()
+		Events[service] = *e
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"message": "not implemented",
+		"message": "event created",
+		"service": service,
+		"status":  status,
 	})
 }
 
+// PUT /events/:id
 func updateEvent(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "not implemented",
@@ -33,12 +59,12 @@ func updateEvent(c *gin.Context) {
 }
 
 func main() {
-	r := gin.Default()
+	router := gin.Default()
 
-	r.GET("/events", getEvents)
-	r.GET("/events/:id", getEvent)
-	r.POST("/events", createEvent)
-	r.PUT("/events/:id", updateEvent)
+	router.GET("/events", getEvents)
+	router.GET("/events/:id", getEvent)
+	router.POST("/events", createEvent)
+	router.PUT("/events/:id", updateEvent)
 
-	r.Run(":8080")
+	router.Run()
 }
