fc3760183a873448c8828f4ae5a8845e17e72294
Mike Hoskins <mrh@devopsdreams.io>
2020-05-29 00:19:16 -0400

    update for new response format

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -8,15 +8,15 @@ HOST="localhost:8080/api"
 http ${HOST}/events
 
 # Post events
-http POST ${HOST}/events service=foo status=bar
-http POST ${HOST}/events service=baz status=qux
+http POST ${HOST}/events service=foo status=up description="doing ok"
+http POST ${HOST}/events service=bar status=down description="kinda broke"
 
 # List events
 http ${HOST}/events
 
 # List event detail
-ID=$(http ${HOST}/events | jq -r '.events[0]')
-http ${HOST}/events/${ID}
+SVC=$(http ${HOST}/events | jq -r '.events[0].service')
+http ${HOST}/events/${SVC}
 
 # Update event
-http PUT ${HOST}/events/${ID} service=foo status=CHANGED
+http PUT ${HOST}/events/${SVC} status=maintenance description="UPDATED"
