c3cf36e10fa878f63bdc851fb44834aa691ab66e
Mike Hoskins <mrh@devopsdreams.io>
2020-09-13 12:02:06 -0400

    wire up initial components

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import TwitterFeed from "./TwitterFeed";
 // import StatusSummary from "./StatusSummary";
-// import EventList from "./EventList";
+import EventList from "./EventList";
 // import "./App.css";
 
 const App = () => {
   return (
-    <div className="container">
-      <TwitterFeed />
+    <div className="columns">
+      <div className="column is-two-thirds">
+        <EventList />
+      </div>
+      <div className="column">
+        <TwitterFeed />
+        {/* <StatusSummary /> */}
+      </div>
     </div>
   );
 };
